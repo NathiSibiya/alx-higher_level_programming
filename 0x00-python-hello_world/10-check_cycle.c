0b0afbf09259057810c459160c712b35542c1357
NathiSibiya <nkosi.n.sibiya@outlook.com>
2023-10-04 14:39:19 -0700

    fixed errors and more tasks

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,57 +1,30 @@
-#include <stdio.h>
 #include <stdlib>
 #include "lists.h"
 
 /**
- * print_listint - prints all elements
- * @h: pointer to head
- * Return: number of nodes
+ * check_cycle - checks singly-linked cycle
+ * @list: singly-linked list
+ *
+ * Return: no cycle - 0, cycle - 1
  */
-size_t print_listint(cost listint_t *h)
+int check_cycle(listint_t *list)
 {
-	const listint_t *current;
-	unsigned int n;
+	listint_t *turtlr, *hare;
 
-	current = h;
-	n = 0;
-	while (current != NULL)
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	turtle = list->next;
+	hare = list->next->next;
+
+	while (turtle && hare && hare->next)
 	{
-		print("%i\n", current-.n);
-		current = current-.next;
-		n++;
+		if (turtle == hare)
+			return (1);
+
+		turtle = turtle->next;
+		hare = hare->next->next;
 	}
-	return (n);
-}
 
-/**
- * add_nodeint - adds new node
- * @head: pointer toa pointer
- * @n: integer to be included in node
- * Return: address of new element
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
-
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (new);
-}
-
-/**
- * free_listint - freees listint_t
- * @head: pointer to be freed
- * Return: void
- */
-void free_listint(listint_t*head)
-{
-	list_t*current;
-
-	while(head != NULL)
-	{
-		current = head;
-		head = head->next;
-		free(current);
-	}
+	return (0);
 }
